88bcd84541bbfe35704f981e3ec95ef62ad2a1c9
Dan Seminara <japaneseforgaming@gmail.com>
2022-05-09 13:24:06 -0400

    Done!

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -1,5 +1,17 @@
-function fibonacci(num) {
-  // type your code here
+function fibonacci(no) {
+  
+
+	if (no === 0) { return 0 }
+	if (no === 1) { return 1 }
+	if (no === 2) { return 1 }
+
+	let fib = [0, 1, 2]
+
+	for (let n = 3; n < no; n++) {
+		let fib2 = [fib[1], fib[2], (fib[1]+fib[2])]
+		fib = fib2
+		}
+		return fib[2]
 }
 
 if (require.main === module) {
